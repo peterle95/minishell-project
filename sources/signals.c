5aee966cba00b659df4218385abd740ab1d8d07f
Peter Moelzer <moelzerpeter@gmail.com>
2024-06-28 12:31:18 +0200

    commented out signals(they work), cleaned out some comments

--- sources/signals.c
+++ sources/signals.c
@@ -6,33 +6,13 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 12:00:49 by pmolzer           #+#    #+#             */
-/*   Updated: 2024/06/19 12:00:49 by pmolzer          ###   ########.fr       */
+/*   Updated: 2024/06/28 12:29:56 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-/* static void handle_SIGINT(int sig)
-{
-    printf("Caught signal SIGINT %d\n", sig);
-    // Handle SIGINT (Ctrl + C)
-    rl_replace_line("", 0);
-    ft_putstr_fd("\n", 1);
-    rl_on_new_line();
-    rl_redisplay();
-}
-
-static void handle_SIGQUIT(int sig)
-{
-    printf("Caught signal SIGQUIT %d\n", sig);
-    // Handle SIGQUIT (Ctrl + \)
-    rl_replace_line("", 0);
-    ft_putstr_fd("Quit: ", 1);
-    ft_putnbr_fd(sig, 1);
-    ft_putstr_fd("\n", 1);
-} */
-
-void    signal_handeling(int sig)
+void    signal_handling(int sig)
 {
     if (sig == SIGINT)
     {
